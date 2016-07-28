aeafbf6490143c2066cd00a19abcd15d2b2f4ac5
sconan96 <wnrdjsj5755@naver.com>
2016-07-28 11:16:41 +0800

    Update

--- app/src/main/java/com/example/alab/horseracing/MainActivity.java
+++ app/src/main/java/com/example/alab/horseracing/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alab.horseracing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton button02 = (ImageButton) findViewById(R.id.startbutton);
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Horseselect.class);
+                startActivity(intent);
+            }
+        });
     }
 }
